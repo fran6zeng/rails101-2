17dd42042925e4976fec7263b4366c06f81eed99
fran6 <fran6zeng@gmail.com>
2017-04-15 03:56:27 +0800

    upload

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
 
-    def render_post_description(post)
-      simple_format(post.description)
+    def render_post_content(post)
+      simple_format(post.content)
     end
 end
